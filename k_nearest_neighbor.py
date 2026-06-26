a8d99aed35e0c1170b4aa403525e214bc0913c79
Mohammed Rahil Ahmed <ahmed_morahil@yahoo.com>
2026-06-27 00:09:54 +0300

    Implement KNN classifier on UCI breast cancer dataset

--- k_nearest_neighbor.py
+++ k_nearest_neighbor.py
@@ -1,15 +1,9 @@
 import numpy as np
 from math import sqrt
-import matplotlib.pyplot as plt
 import warnings
-from matplotlib import style
 from collections import Counter
-style.use('fivethirtyeight')
-
-dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
-new_features = [5,7]
-
-
+import pandas as pd
+import random
 
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= k:
@@ -22,14 +16,38 @@ def k_nearest_neighbors(data, predict, k=3):
             distances.append([euclidean_distance, group])
 
     votes = [i[1] for i in sorted(distances)[:k]]
-    print(Counter(votes).most_common(1))
+    #print(Counter(votes).most_common(1))
     vote_result = Counter(votes).most_common(1)[0][0]
 
     return vote_result
 
-result = k_nearest_neighbors(dataset, new_features, k=3)
-print(result)
+df = pd.read_csv('breast-cancer-wisconsin.data', names=['id','clump_thickness','uniform_cell_size','uniform_cell_shape',
+                'marginal_adhesion','single_epithelial_size','bare_nuclei',
+                'bland_chromatin','normal_nucleoli','mitoses','class'])
+df.replace('?', -99999, inplace=True) #replace missing data with outlier
+df.drop(['id'], axis=1, inplace=True) #drop id column as it is not useful for prediction
+full_data = df.astype(float).values.tolist() #convert dataframe to list of lists
+random.shuffle(full_data) #shuffle the data to ensure randomness
 
-[[plt.scatter(ii[0],ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-plt.scatter(new_features[0], new_features[1], s=100, color='g')
-plt.show()
+test_size = 0.2
+train_set = {2:[], 4:[]} #create a dictionary to hold the training data
+test_set = {2:[], 4:[]} #create a dictionary to hold the testing data
+train_data = full_data[:-int(test_size*len(full_data))] #split the data into training and testing sets
+test_data = full_data[-int(test_size*len(full_data)):] #split the data into training and testing sets
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1]) #append the features to the corresponding class in the training set
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1]) #append the features to the corresponding class in the testing set
+
+correct = 0
+total = 0
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5) #make prediction on the test data
+        if group == vote:
+            correct += 1
+        total += 1
+
+print('Accuracy:', correct/total) #print the accuracy of the model
